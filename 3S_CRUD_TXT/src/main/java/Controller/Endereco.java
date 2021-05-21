0dee79de8080789a5dc16d7726c2d4f07615fdd6
Paulo H. Grillo <paulo.grillo@outlook.com>
2021-05-21 09:30:12 -0300

    Alterado nome da pasta raiz.

---
+++ 3S_CRUD_TXT/src/main/java/Controller/Endereco.java
@@ -0,0 +1,84 @@
+package Controller;
+
+public class Endereco {
+
+    // Atributos
+    private String logradouro;
+    private String numero;
+    private String bairro;
+    private String cep;
+    private String complemento;
+    private String cidade;
+    private String estado;
+
+    // Construtores
+    public Endereco() {
+    }
+
+    public Endereco(String logradouro, String numero, String bairro, String cep, String complemento, String cidade, String estado) {
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.bairro = bairro;
+        this.cep = cep;
+        this.complemento = complemento;
+        this.cidade = cidade;
+        this.estado = estado;
+    }
+
+    // Getter's e Setter's
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
